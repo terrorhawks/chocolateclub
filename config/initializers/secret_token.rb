63df87c44155029260934d33386f4f605929a038
David Clarke <terrorhawks@gmail.com>
2014-07-11 10:17:32 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chocolateclub::Application.config.secret_token = 'acd34fde7a978c275b71f317d3ffcccb6b377c7d205b38414cd8e3fb282fdd7b5066e331967f6afa7fdde5e5213cdde4029754b078a647b2c5b18cd07a1a0388'
